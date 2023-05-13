e59633e364c19c79e8e6368eaf5ec19362bccbaa
sauvepro <unsauvechriss@gmail.com>
2023-05-13 23:58:44 +0300

    validating user iniputs

--- middlewares/user.validation.js
+++ middlewares/user.validation.js
@@ -1,11 +1,11 @@
-const joi = require("joi")
+const joi = require("joi") // module for validating inputs
 const validateUserInput = (req, res, next) =>{
     const schema = joi.object({
         username: joi.string().required().min(6),
         password: joi.string().required().min(8)
     })
 
-    const {error} = schema.validate(req.body)
+    const {error} = schema.validate(req.body) 
     if(error) {
         return res.status(400).json({status: 400, errorMessage: error.details[0].message})
     }
