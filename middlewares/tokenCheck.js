dd10fddf43ddce44358f7fc2fa55e557e6862fb0
sauvepro <unsauvechriss@gmail.com>
2023-05-14 00:25:39 +0300

    all commits

--- middlewares/tokenCheck.js
+++ middlewares/tokenCheck.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken"); // model of  Web tokens
 require("dotenv").config();
 const verifyToken = async (req, res, next) => {
   const token = req.cookies.authToken;
@@ -6,7 +6,7 @@ const verifyToken = async (req, res, next) => {
     return res.status(403).json({ status: 403, message: "No token found" }); //checking if ther is available token
   }
   try {
-    const payload = await jwt.verify(token, process.env.JWT_SECRET);
+    const payload = await jwt.verify(token, process.env.JWT_SECRET); // verfiying token
     next();
     return payload;
   } catch (error) {
